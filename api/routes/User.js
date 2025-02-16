d0c22607fd1acdc0372d6913899d578156ef91b7
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-16 22:20:24 +0000

    User registeration API setup

--- api/routes/User.js
+++ api/routes/User.js
@@ -24,4 +24,35 @@ userRoute.post('/login',
     })
 );
 
+//Register User route
+userRoute.post('/',
+    AsyncHandler(async (req, res) => {
+        const { name, email, password } = req.body;
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            res.status(400);
+            throw new Error('User Already Exists');
+        }else{
+            const user = await User.create({
+                name,
+                email,
+                password
+            });
+            if(user){
+                res.status(201).json({
+                    _id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    isAdmin: user.isAdmin,
+                    token: null,
+                    createdAt: user.createdAt
+                })
+            }else{
+                res.status(400);
+                throw new Error('Invalid User Data');
+            }
+        }
+    })
+);
+
 module.exports = userRoute;
