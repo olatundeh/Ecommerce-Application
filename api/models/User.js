2e1df0b44a1d46899dd142b854b042e7d5210361
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-16 22:39:07 +0000

    Implemented Hash password before saving

--- api/models/User.js
+++ api/models/User.js
@@ -19,4 +19,14 @@ userSchema.methods.matchPassword =  async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
+//Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 module.exports = mongoose.model("User", userSchema)
