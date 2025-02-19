b5080041a1ae54d2abd8f35dfd257c0c137706ff
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-19 22:11:37 +0000

    Updated Auth middleware to validate if user has key

--- api/middleware/Auth.js
+++ api/middleware/Auth.js
@@ -2,5 +2,17 @@ const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 
 const protect = asyncHandler(async (req, res, next) => {
-
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decodedToken.id).select('-password');
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Not Authorized, Token Failed');
+        }
+    }
 });
