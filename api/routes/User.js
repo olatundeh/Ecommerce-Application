d10b3a6b662f00cb0558c9d7f2935fd73f3c6c17
Olatunde Haroun <haroun.ola@gmail.com>
2025-02-21 22:32:42 +0000

    Implemented User profile data update

--- api/routes/User.js
+++ api/routes/User.js
@@ -57,7 +57,7 @@ userRoute.post('/',
     })
 );
 
-//Profile route
+//get auth Profile details
 userRoute.get('/profile',
     protect,
     AsyncHandler(async (req, res) => {
@@ -77,4 +77,29 @@ userRoute.get('/profile',
     })
 );
 
+//user profile data update
+userRoute.put('/profile', protect,
+    AsyncHandler(async (req, res) => {
+        const user = await User.findById(req.user._id);
+        if(user){
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            if(req.body.password){
+                user.password = req.body.password;
+            }
+            const updatedUser = await user.save();
+            res.json({
+                _id: updatedUser.id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                isAdmin: updatedUser.isAdmin,
+                token: generateToken(updatedUser._id),
+                createdAt: updatedUser.createdAt
+            });
+        }else{
+            res.status(404);
+            throw new Error('User Not Found');
+        }
+    })
+);
 module.exports = userRoute;
