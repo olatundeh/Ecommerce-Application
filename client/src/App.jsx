956728cbeeae46fe747feffdfa0bd0f137b3cb2e
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-04 22:06:23 +0000

    Add Footer component to App layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Layouts/Navbar'
+import Footer from './Layouts/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
+
+      <Footer />
     </>
   )
 }
