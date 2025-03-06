dd114eab02b03e9c049b83e7a912cc1eff3fac00
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-06 00:00:17 +0000

    Refactor import statements in Layout component to use ES6 syntax

--- client/src/Layouts/Layouts.jsx
+++ client/src/Layouts/Layouts.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import Footer from "./Footer"
 import Navbar from "./Navbar"
-=======
-const { default: Footer } = require("./Footer");
-const { default: Navbar } = require("./Navbar");
->>>>>>> f2de63300e484d0cb87d44a65b88030549cd5e1e
 
 const Layout = ({children}) => {
   return (
