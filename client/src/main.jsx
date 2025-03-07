47a542bbe10066e46cf268ce300a8485283dda52
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-07 12:19:36 +0000

    Add ProductDetail component and route to main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import ProductDetail from './pages/ProductDetail.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
+        <Route exact path="/Detail" element={<ProductDetail />} />
       </Routes>
     </Router>
   </StrictMode>,
---
+++ client/src/pages/ProductDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../Layouts/Layouts';
+
+function ProductDetail() {
+
+    return (
+      <Layout>
+        Product Details
+      </Layout>
+    )
+}
+
+export default ProductDetail;
