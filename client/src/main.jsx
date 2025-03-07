79bf4ddc54c506ccbe5863a70519f37c09baf0c6
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-07 11:56:51 +0000

    Add routing to main application using react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
