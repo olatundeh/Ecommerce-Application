d902fe6180b9c1a3244383128e8ca454b0e0e54a
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-17 22:39:34 +0000

    Update ProductDetail route to use dynamic product ID parameter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/Detail" element={<ProductDetail />} />
+          <Route exact path="/product/:id" element={<ProductDetail />} />
         </Routes>
       </Router>
     </>
