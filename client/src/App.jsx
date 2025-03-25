b571efacd0398eed0195f316b37c1d41c7342cbd
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-25 23:12:53 +0000

    Add routes for Login and Register components in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductDetail from './pages/ProductDetail'
 import Home from './pages/Home'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/product/:id" element={<ProductDetail />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
         </Routes>
       </Router>
     </>
